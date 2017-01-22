d06edac6ce786fa79f685e23390ecf0e60a15eab
Jonathan Gianfreda <jonathangianf2@gmail.com>
2017-01-22 11:19:51 +0100

    added game over state

---
+++ front/js/states/gameover.js
@@ -0,0 +1,7 @@
+define(['js/states/gameover/preload', 'js/states/gameover/create', 'js/states/gameover/update'], function(preload, create, update) {
+    return {
+        'preload': preload,
+        'create': create,
+        'update': update
+    }
+});
---
+++ front/js/states/gameover/preload.js
@@ -0,0 +1,17 @@
+define(['js/phaser', 'js/res'], function(phaser, res) {
+
+    var game;
+
+    return function() {
+        game = phaser.getGame();
+
+        game.stage.backgroundColor = '#000000';
+
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
+
+        game.load.image('buttons.play', res.buttons.play);
+    }
+});
